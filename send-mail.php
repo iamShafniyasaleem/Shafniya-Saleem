67745b3f159b99a91d53fb94a8faaabedf6a4e50
iamShaf <55636938+iamShafniyasaleem@users.noreply.github.com>
2021-06-22 00:15:20 +0530

    Add files via upload

---
+++ send-mail.php
@@ -0,0 +1,15 @@
+<?
+$Email=$_POST['Email'];
+$subject=$_POST['Name'];
+$message=$_POST['message'];
+$body .= "Email: " . $Email . "\n";
+$body .= "subject: " . $subject . "\n";
+$body .= "Message: " . $message . "\n";
+mail("shafniyathedeveloper@gmail.com","From One-Page Site",$body);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script>alert("Your email has been sent. Thank You!");</script>
+<meta HTTP-EQUIV="REFRESH" content="0; url=index.html">
+</head>
